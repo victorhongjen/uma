d1400d9a6cfb8dff7c2e90053189aace8ea82be6
Victor Wang <hongjenwang@gmail.com>
2022-07-23 01:22:17 -0700

    change faq section to accordion style

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let faqs = document.querySelectorAll('.faq-single')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
@@ -10,3 +11,11 @@ window.onscroll = () => {
      menu.classList.remove('fa-times')
      navbar.classList.remove('nav-toggle')
 }
+
+console.log(faqs)
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
